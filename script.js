126efaef5c12b80d10baa8357df46eec1214a69f
PrincessEmina <pemina342@gmail.com>
2025-04-04 17:04:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -151,3 +151,11 @@ document.addEventListener("DOMContentLoaded", () => {
   setInterval(createSparkle, 300)
 })
 
+
+function playVideo() {
+    const videoContainer = document.querySelector('.video-container');
+    videoContainer.innerHTML = `
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/sPPukcEo5v0?si=6TWp7m-dJTiAN-O4&amp;start=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    `;
+}
+
